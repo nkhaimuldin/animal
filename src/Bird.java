3185b0634101f81372f5f4be68aaeb84b9befa2d
Nursltan Khaimuldin <n.khaimuldin@astanait.edu.kz>
2024-12-21 09:25:53 +0500

    implemented the constructor of Bird class and its getters and setters

--- src/Bird.java
+++ src/Bird.java
@@ -1,2 +1,28 @@
 public class Bird extends Animal {
+    private String something;
+
+
+    public Bird() {
+        super();
+    }
+
+    public Bird(String name, int age, boolean gender, String something) {
+        super(name, age, gender);
+        this.something = something;
+    }
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+
+    @Override
+    public String toString() {
+        return "Bird{" +
+                "something='" + something + '\'' +
+                '}';
+    }
 }
